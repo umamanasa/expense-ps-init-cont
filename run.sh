bfe497f3b9b5215c2c92debaa409458bd19ae048
umamanasa <umamanasa49@gmail.com>
2024-05-06 13:58:07 +0530

    kubernetes containers

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-aws ssm describe-parameters
+for parameter in $PARAMETERS; do
+  ssm_parameter=$(echo $parameter | awk -F , '{print $1}')
+  app_parameter=$(echo $parameter | awk -F , '{print $2}')
+  echo export ${app_parameter}=\"$(aws ssm get-parameter  --name $ssm_parameter --with-decryption | jq .Parameter.Value | sed -e 's/"//g')\" >>/common/parameters
+done
+
+cat /common/parameters
